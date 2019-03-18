c1d4c8f43b2518a25dc3dee67ba196fbc1918849
KARINA  LUMI DUQUE KURODA <17.00709-7@maua.br>
2019-03-18 11:43:34 -0300

    Projeto criado com Eclipse

---
+++ Prática2/eclipse/OlaMundoEclipse/src/MinhaClasseJava.java
@@ -0,0 +1,14 @@
+
+public class MinhaClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 24;
+		String nome = "Emerson";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		
+
+	}
+
+}
